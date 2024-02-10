cd6edab9d2baa70844c5af2b2e5aa56fd2e1140d
Anton <antonklim6@gmail.com>
2024-02-10 11:19:35 +0100

    update form

---
+++ src/js/2-form.js
@@ -0,0 +1,37 @@
+const LS_KEY = "feedback-form-state"
+
+const formElement = document.querySelector(".feedback-form")
+const textareaElement = document.querySelector("textarea")
+
+function readFormData (form) {
+    const message = form.message.value
+    const email = form.email.value
+    return {
+        email,
+        message
+    }
+}
+
+
+formElement.addEventListener('submit', (event) => {
+    
+    console.log(readFormData(formElement))
+    event.preventDefault()
+    localStorage.removeItem(LS_KEY)
+    formElement.reset()
+})
+
+formElement.addEventListener('input', (event) => {
+    const data = readFormData(event.currentTarget)
+    localStorage.setItem(LS_KEY, JSON.stringify(data))
+
+})
+
+
+
+const rawData = localStorage.getItem(LS_KEY)
+if(rawData) {
+    const data = JSON.parse(rawData)
+    formElement.email.value = data.email
+    formElement.message.value = data.message
+}
